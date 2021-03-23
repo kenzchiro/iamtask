137144928cc6998ec4d7acc04dd54d90049a4122
Nattawut Khuadplod <38815327+kenzchiro@users.noreply.github.com>
2021-03-24 02:18:22 +0700

    Update problem1.go

--- src/problem1.go
+++ src/problem1.go
@@ -9,8 +9,9 @@ func isPalindrome(input string) bool {
 word:= strings.ToLower(input)
 l:=0
 r:=len(input)-1
-
+// chack from left->right and right->left
 for l < r {
+   //compare char
    if word[l] != word[r]{
      return false
    }
